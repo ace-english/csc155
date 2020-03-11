793281865c2dc08f7a1c5d8e0d0810d8932abc0f
Ace English <Ace English>
2020-03-11 13:47:07 -0700

    axis kinda working

--- src/a2/axisVertShader.glsl
+++ src/a2/axisVertShader.glsl
@@ -6,11 +6,23 @@ layout (location=1) in vec2 texCoord;
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 
-out vec4 varyingColor;
+out vec4 color;
 
 void main(void){
 	gl_Position = proj_matrix * mv_matrix * vec4(position,1.0);
+	/*
+	int r=1;
+	int g=1;
+	int b=1;
+	if(position.x==0){
+		r=0;
+	}if(position.y==0){
+		g=0;
+	}if(position.z==0){
+		b=0;
+	}
+	varyingColor = vec4(r,g,b,1.0);
+	*/
 	
-
-	varyingColor = vec4(position,1.0);
+	varyingColor = vec4(position,1.0)*0.5 + vec4(0.5, 0.5, 0.5, 0.5);
 }
