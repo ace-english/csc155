7bdd7fd710ef4d33f3525302d06d4bb53992a244
Ace English <Ace English>
2020-05-02 17:46:24 -0700

    normals work

--- src/a4/phongFragShader.glsl
+++ src/a4/phongFragShader.glsl
@@ -3,11 +3,17 @@
 in vec3 varyingNormal;
 in vec3 varyingLightDir;
 in vec3 varyingVertPos;
+in vec3 originalVertex;
+in vec3 varyingTangent;
 in vec3 varyingHalfVector;
-in vec4 shadow_coord;
 in vec2 tc;
+in vec4 shadow_coord;
 
 out vec4 fragColor;
+
+layout (binding=0) uniform sampler2D t;
+layout (binding=1) uniform sampler2D s;
+layout (binding=2) uniform sampler2DShadow shadowTex;
 
 struct PositionalLight
 {	vec4 ambient;  
@@ -31,19 +37,36 @@ uniform mat4 proj_matrix;
 uniform mat4 norm_matrix;
 uniform mat4 shadowMVP;
 
-layout (binding=0) uniform sampler2D t;
-layout (binding=1) uniform sampler2D s;
-layout (binding=2) uniform sampler2DShadow shadowTex;
+
+vec3 calcNewNormal()
+{
+	vec3 normal = normalize(varyingNormal);
+	vec3 tangent = normalize(varyingTangent);
+	tangent = normalize(tangent - dot(tangent, normal) * normal);
+	vec3 bitangent = cross(tangent, normal);
+	mat3 tbn = mat3(tangent, bitangent, normal);
+	vec3 retrievedNormal = texture(s,tc).xyz;
+	retrievedNormal = retrievedNormal * 2.0 - 1.0;
+	vec3 newNormal = tbn * retrievedNormal;
+	newNormal = normalize(newNormal);
+	return newNormal;
+}
 
 void main(void)
 {	// normalize the light, normal, and view vectors:
 	vec3 L = normalize(varyingLightDir);
-	vec3 N = normalize(varyingNormal);
 	vec3 V = normalize(-varyingVertPos);
+	
+	vec3 N = calcNewNormal();
 	
 	float cosTheta = dot(L,N);
 	vec3 H = normalize(varyingHalfVector);
-	float cosPhi = dot(H,N);
+	
+	// compute light reflection vector, with respect N:
+	vec3 R = normalize(reflect(-L, N));
+	
+	// angle between the view vector and reflected light:
+	float cosPhi = dot(V,R);
 		
 	vec3 ambient = ((globalAmbient * material.ambient) + (light.ambient * material.ambient)).xyz;
 	vec3 diffuse = light.diffuse.xyz * material.diffuse.xyz * max(cosTheta,0.0);
