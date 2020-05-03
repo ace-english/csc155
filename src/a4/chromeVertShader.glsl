2fe2513ce064181e0c345ab1b1452acefb145dce
Ace English <Ace English>
2020-05-02 18:38:43 -0700

    all variables accounted for

--- src/a4/chromeVertShader.glsl
+++ src/a4/chromeVertShader.glsl
@@ -3,6 +3,9 @@
 layout (location=0) in vec3 position;
 layout (location=1) in vec2 texCoord;
 layout (location=2) in vec3 normal;
+out vec3 vNormal;
+out vec3 vVertPos;
+
 
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
@@ -13,6 +16,8 @@ out vec2 tc;
 
 void main(void)
 {
+	vVertPos = (mv_matrix * vec4(position,1.0)).xyz;
+	vNormal = (norm_matrix * vec4(normal,1.0)).xyz;
 	gl_Position = proj_matrix * mv_matrix * vec4(position,1.0);
 	tc = texCoord;
 }
