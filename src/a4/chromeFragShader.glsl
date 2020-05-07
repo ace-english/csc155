63ce3471164f9b7b8ae4c963c7db0fe0f45a5550
Ace English <Ace English>
2020-05-06 19:50:27 -0700

    frag has variables

--- src/a4/chromeFragShader.glsl
+++ src/a4/chromeFragShader.glsl
@@ -1,10 +1,32 @@
 #version 430
 
 in vec3 varyingNormal;
+in vec3 varyingLightDir;
 in vec3 varyingVertPos;
+in vec3 varyingTangent;
+in vec3 originalVertex;
+in vec3 varyingHalfVector;
 in vec3 vertEyeSpacePos;
 out vec4 fragColor;
 
+
+struct PositionalLight
+{	vec4 ambient;  
+	vec4 diffuse;  
+	vec4 specular;  
+	vec3 position;
+};
+
+struct Material
+{	vec4 ambient;  
+	vec4 diffuse;  
+	vec4 specular;  
+	float shininess;
+};
+
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 uniform mat4 norm_matrix;
