bce68beb3ee9cd00616f4564342a82493d96a605
Ace English <Ace English>
2020-05-07 20:28:26 -0700

    shaders compile

--- src/a4/3dVertShader.glsl
+++ src/a4/3dVertShader.glsl
@@ -1,20 +1,11 @@
 #version 430
 
-layout (location = 0) in vec3 vertPos;
-layout (location = 1) in vec2 texCoord;
-layout (location = 2) in vec3 vertNormal;
-layout (location = 3) in vec3 vertTangent;
-
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
 out vec3 varyingNormal;
+out vec3 originalPosition;
 out vec3 varyingLightDir;
 out vec3 varyingVertPos;
-out vec3 varyingTangent;
-out vec3 originalVertex;
-out vec3 varyingHalfVector;
-out vec2 tc;
-
-layout (binding=0) uniform sampler3D s;
-
 
 struct PositionalLight
 {	vec4 ambient;
@@ -37,14 +28,12 @@ uniform mat4 proj_matrix;
 uniform mat4 norm_matrix;
 uniform mat4 texRot_matrix;
 
-void main(void)
-{	varyingVertPos = (mv_matrix * vec4(vertPos,1.0)).xyz;
-	varyingLightDir = light.position - varyingVertPos;
-	tc = texCoord;
-	originalVertex = vec3(texRot_matrix * vec4(position,1.0)).xyz;
-	
-	varyingNormal = (norm_matrix * vec4(vertNormal,1.0)).xyz;
-	varyingTangent = (norm_matrix * vec4(vertTangent,1.0)).xyz;
+layout (binding=0) uniform sampler3D s;
 
-	gl_Position = proj_matrix * mv_matrix * vec4(vertPos,1.0);
+void main(void)
+{	varyingNormal = (norm_matrix * vec4(normal,1.0)).xyz;
+	originalPosition = vec3(texRot_matrix * vec4(position,1.0)).xyz;
+	varyingVertPos = (mv_matrix * vec4(position,1.0)).xyz;
+	varyingLightDir = light.position - varyingVertPos;
+	gl_Position = proj_matrix * mv_matrix * vec4(position,1.0);
 }
